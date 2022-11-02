bfd89fce2de76b001a3bdc751af09bc691545357
Elumenix <89927445+Elumenix@users.noreply.github.com>
2022-11-02 12:23:13 -0400

    completely finished gif finder

--- Gif Finder/js/main.js
+++ Gif Finder/js/main.js
@@ -84,11 +84,11 @@ function searchButtonClicked(){
         // 9 - Start building an HTML string we will display to the user
         let results = obj.data;
         console.log("results.length = " + results.length);
-        let bigString = "<p><i>Here are " + results.length + " results for '" + displayTerm + "'</i></p>";
-
+        let bigString = "";
+        
         // 10 - loop through the array of results
-        for (let i = 0; i < results.length; i++) {
-            let result = results[i];
+        for (const element of results) {
+            let result = element;
 
             // 11 - get the URL to the GIF
             let smallURL = result.images.fixed_width_downsampled.url;
@@ -126,7 +126,8 @@ function searchButtonClicked(){
         document.querySelector("#content").innerHTML = bigString;
 
         // 17 - update the status
-        document.querySelector("#status").innerHTML = "<b>Success!</b>";
+        document.querySelector("#status").innerHTML = "<b>Success!</b> <p><i>Here are " + results.length + " results for '" + displayTerm + "'</i></p>";
+        ;
     }
 
     function dataError(e){
