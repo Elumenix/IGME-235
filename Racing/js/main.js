bcbddb366909be03591afc216068b82a935aaf4d
Elumenix <89927445+Elumenix@users.noreply.github.com>
2022-11-30 18:59:12 -0500

    Finally figured out how to load and frame an asset

--- Racing/js/main.js
+++ Racing/js/main.js
@@ -5,41 +5,38 @@ const app = new PIXI.Application(
             backgroundColor: 0xAAAAAA
         }
     );
+    assets = PIXI.Assets;
+
 
     // Canvas is appended to page after the page loads
     window.onload = function() {
-        document.querySelector("div").appendChild(app.view)
+        document.querySelector("div").appendChild(app.view);
     }
 
-
-    // Sprite Sheet data
-    const cars = {
-        frames: {
-            car: {
-                frame: { x: 0, y:0, w:16, h:16 },
-                sourceSize: { w: 16, h: 16 },
-                spriteSourceSize: { x: 0, y: 0, w: 16, h: 16 }
-        }
-    },
-    meta: {
-        image: 'images/Mini Pixel Pack 2/Cars/Player_green (16 x 16).png',
-        format: 'RGBA8888',
-        size: {w: 128, h: 16},
-        scale: 1
-    }
-}
-
-    //let car = PIXI.BaseTexture.from(car);
-    const car = PIXI.Sprite.from('images/Mini Pixel Pack 2/Cars/Player_green (16 x 16).png');
-    //const car = PIXI.TilingSprite.from('images/Mini Pixel Pack 2/Cars/Player_green (16 x 16).png', 16);
+    
 
 
-    setup();
+   
+    let car;
+
+    const loadedTexture = assets.load("images/Mini Pixel Pack 2/Cars/Player_green (16 x 16).png");
+
+    loadedTexture.then((resolvedTexture) => {
+        const rect = new PIXI.Rectangle(0,0,16,16);
+        resolvedTexture.frame = rect;
+        car = new PIXI.Sprite(resolvedTexture);
+        console.log(car);
+        setup();
+    });
 
 function setup() {
     let stage = app.stage;
+
+
     stage.addChild(car);
     car.anchor.set(0.5);
+    car.width = 32;
+    car.height = 32
     car.x = app.screen.width / 2;
     car.y = app.screen.height / 2;
 }
