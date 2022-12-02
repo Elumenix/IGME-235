72b1bb352f8caf76a5bf277c73b75fe0d6e98faa
Elumenix <89927445+Elumenix@users.noreply.github.com>
2022-12-02 01:30:03 -0500

    Finished setting up a basic track

--- Racing/js/main.js
+++ Racing/js/main.js
@@ -34,15 +34,46 @@ const app = new PIXI.Application(
 
    
     let car;
+    let start;
+    let finish;
+    let straight1;
+    let straight2;
+    let straight3;
 
-    const loadedTexture = assets.load("images/Mini Pixel Pack 2/Cars/Player_green (16 x 16).png");
+    assets.add('car', 'images/Mini Pixel Pack 2/Cars/Player_green (16 x 16).png');
+    assets.add('start', 'images/Tracks/Start.png');
+    assets.add('finish', 'images/Tracks/Finish.png');
+    assets.add('turn', 'images/Tracks/Road_01_Tile_01.png');
+    assets.add('straight', 'images/Tracks/Road_01_Tile_03.png');
 
-    loadedTexture.then((resolvedTexture) => {
+
+
+    const loadedTextures = assets.load('car');
+
+    // Unfortunately messy as I needed a way for car to run by itself
+    loadedTextures.then((resolvedTexture) => {
+        // Gets the cars sprite
         const rect = new PIXI.Rectangle(0,0,16,16);
         resolvedTexture.frame = rect;
         car = new PIXI.Sprite(resolvedTexture);
-        console.log(car);
-        setup();
+
+        let nextLoadedTextures = assets.load(['start', 'finish', 'turn', 'straight']);
+        nextLoadedTextures.then((resolvedTexture) => {
+            straight1 = PIXI.Sprite.from(resolvedTexture.straight);
+            straight2 = PIXI.Sprite.from(resolvedTexture.straight);
+            straight3 = PIXI.Sprite.from(resolvedTexture.straight);
+            start = PIXI.Sprite.from(resolvedTexture.start);
+            finish = PIXI.Sprite.from(resolvedTexture.finish);
+
+            turn = PIXI.Sprite.from(resolvedTexture.turn);
+
+        
+
+
+            setup();
+        });
+
+
     });
 
 function setup() {
@@ -50,13 +81,61 @@ function setup() {
     app.ticker.add(gameLoop);
 
 
+    stage.addChild(straight1);
+    stage.addChild(straight2)
+    stage.addChild(straight3);
+    stage.addChild(turn);
+    stage.addChild(start);
+    stage.addChild(finish);
 
     stage.addChild(car);
+
+
     car.anchor.set(0.5);
     car.width = 32;
     car.height = 32
     car.x = app.screen.width / 2;
-    car.y = app.screen.height / 2;
+    car.y = app.screen.height / 2 + 30;
+
+    straight1.anchor.set(0.5);
+    straight1.x = app.screen.width / 2;
+    straight1.y = app.screen.height / 2 - 40;
+    straight1.scale.x = .25;
+    straight1.scale.y = .25;
+    straight1.angle = 90;
+
+    turn.anchor.set(0.5);
+    turn.x = app.screen.width / 2 + 1;
+    turn.y = app.screen.height / 2 - 179;
+    turn.scale.x = .25;
+    turn.scale.y = .25;
+    turn.angle = 90;
+
+    straight2.anchor.set(0.5);
+    straight2.x = app.screen.width / 2 + 139;
+    straight2.y = app.screen.height / 2 - 179;
+    straight2.scale.x = .25;
+    straight2.scale.y = .25;
+
+    straight3.anchor.set(0.5);
+    straight3.x = app.screen.width / 2 + 267;
+    straight3.y = app.screen.height / 2 - 179;
+    straight3.scale.x = .25;
+    straight3.scale.y = .25;
+
+    start.anchor.set(0.5);
+    start.x = app.screen.width / 2;
+    start.y = app.screen.height / 2;
+    start.scale.x = .0625;
+    start.scale.y = .0625;
+
+    finish.anchor.set(0.5);
+    finish.x = app.screen.width / 2 + 305;
+    finish.y = app.screen.height / 2 - 179;
+    finish.scale.x = .0625;
+    finish.scale.y = .0625;
+    finish.angle = 90;
+
 }
 
 function gameLoop() {
