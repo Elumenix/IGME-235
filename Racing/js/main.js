8efbe8d15d455ba0c9cd6a8f5bd35093586cc2c8
Elumenix <89927445+Elumenix@users.noreply.github.com>
2022-12-06 16:50:44 -0500

    Finished making the track

--- Racing/js/main.js
+++ Racing/js/main.js
@@ -40,6 +40,8 @@ const app = new PIXI.Application(
     let turnAsset;
     let turnAsset2;
     let swerveAsset;
+    let shortAsset;
+    let shrinkAsset;
 
     assets.add('car', 'images/Mini_Pixel_Pack_2/Cars/Player_green_(16x16).png');
     assets.add('start', 'images/Tracks/Start.png');
@@ -48,6 +50,9 @@ const app = new PIXI.Application(
     assets.add('turn2', 'images/Tracks/Road_01_Tile_02.png');
     assets.add('straight', 'images/Tracks/Road_01_Tile_03.png');
     assets.add('swerve', 'images/Tracks/Road_01_tile_06.png');
+    assets.add('short', 'images/Tracks/Road_01_tile_07.png');
+    assets.add('shrink', 'images/Tracks/Road_01_tile_08.png')
+
 
 
 
@@ -60,12 +65,15 @@ const app = new PIXI.Application(
         resolvedTexture.frame = rect;
         car = new PIXI.Sprite(resolvedTexture);
 
-        let nextLoadedTextures = assets.load(['start', 'finish', 'turn', 'straight', 'swerve', 'turn2']);
+        let nextLoadedTextures = assets.load(['start', 'finish', 'turn', 'straight', 'swerve', 'turn2', 'short', 'shrink']);
         nextLoadedTextures.then((resolvedTexture) => {
             straightAsset = PIXI.Sprite.from(resolvedTexture.straight);
             startAsset = PIXI.Sprite.from(resolvedTexture.start);
             finishAsset = PIXI.Sprite.from(resolvedTexture.finish);
+            shortAsset = PIXI.Sprite.from (resolvedTexture.short)
             swerveAsset = PIXI.Sprite.from(resolvedTexture.swerve);
+            shrinkAsset = PIXI.Sprite.from(resolvedTexture.shrink);
+
 
             turnAsset = PIXI.Sprite.from(resolvedTexture.turn);
             turnAsset2 = PIXI.Sprite.from(resolvedTexture.turn2);
@@ -87,25 +95,53 @@ function setup() {
 
 
     // Common Tracks
-    new Road(stage, straightAsset, 0.5, xMid - 500, yMid - 40, .25, .25, 90);
-    new Road(stage, straightAsset, 0.5, xMid - 361, yMid - 179, -.25, -.25);
-    new Road(stage, straightAsset, 0.5, xMid - 233, yMid - 179, .25, .25);
+    new Road(stage, straightAsset, 0.5, xMid - 460, yMid - 65, .25, .25, 90);
+    new Road(stage, straightAsset, 0.5, xMid - 321, yMid - 204, -.25, -.25);
+    new Road(stage, straightAsset, 0.5, xMid - 193, yMid - 204, .25, .25);
+    new Road(stage, swerveAsset, 0.5, xMid - 33, yMid - 236, .25, -.25);
+    new Road(stage, shortAsset, 0.5, xMid + 395, yMid - 267, .25, -.25);
+    new Road(stage, straightAsset, 0.5, xMid + 502, yMid - 128, .25, -.25, 90);
+    new Road(stage, shrinkAsset, 0.5, xMid + 502, yMid, .25, -.25, 90);
+    new Road(stage, shrinkAsset, 0.5, xMid + 502, yMid + 128, .25, -.25, -90);
+    new Road(stage, swerveAsset, 0.5, xMid + 331, yMid + 235, .25, .25);
+    new Road(stage, shortAsset, 0.5, xMid + 203, yMid + 203, .25, .25);
+    new Road(stage, swerveAsset, 0.5, xMid + 75, yMid + 171, .25, .25, 180);
+    new Road(stage, swerveAsset, 0.5, xMid - 117, yMid + 107, .25, .25);
+    new Road(stage, shortAsset, 0.5, xMid - 417, yMid + 266, -.25, -.25);
+    new Road(stage, shortAsset, 0.5, xMid - 288, yMid + 183, .25, .25, 90);
+    new Road(stage, swerveAsset, 0.5, xMid - 492, yMid + 95, .25, .25, 90);
+    new Road(stage, shortAsset, 0.5, xMid - 390, yMid + 266, .25, -.25);
 
-    new Road(stage, swerveAsset, 0.5, xMid - 73, yMid - 211, .25, -.25)
+
+
+
+
+
+
+
+
 
     // Turns
-    new Road(stage, turnAsset, 0.5, xMid - 499, yMid - 179, .25, .25, 90);
-    new Road(stage, turnAsset, 0.5, xMid + 99, yMid - 93, .25, -.25, -90);
-    new Road(stage, turnAsset2, 0.5, xMid + 98, yMid - 243, .25, .25, 90);
-    new Road(stage, turnAsset2, 0.5, xMid + 248, yMid - 93, .25, -.25, -90);
+    new Road(stage, turnAsset, 0.5, xMid - 459, yMid - 204, .25, .25, 90);
+    new Road(stage, turnAsset, 0.5, xMid + 139, yMid - 118, .25, -.25, -90);
+    new Road(stage, turnAsset, 0.5, xMid + 288, yMid - 118, -.25, .25);
+    new Road(stage, turnAsset, 0.5, xMid + 289, yMid - 267, .25, .25, 90);
+    new Road(stage, turnAsset, 0.5, xMid + 502, yMid - 266, .25, .25, 180);
+    new Road(stage, turnAsset2, 0.5, xMid + 138, yMid - 268, .25, .25, 90);
+    new Road(stage, turnAsset, 0.5, xMid + 502, yMid + 266, -.25, .25);
+    new Road(stage, turnAsset2, 0.5, xMid - 288, yMid + 76, -.25, -.25, -180);
+    new Road(stage, turnAsset, 0.5, xMid - 524, yMid + 265, .25, .25);
+    new Road(stage, turnAsset2, 0.5, xMid - 288, yMid + 265, .25, .25, 180);
+
+    
 
     
     // Track Marks
-    new Road(stage, startAsset, 0.5, xMid - 500, yMid, .0625, .0625);
-    new Road(stage, finishAsset, 0.5, xMid - 205, yMid - 179, .0625, .0625, 90);
+    new Road(stage, startAsset, 0.5, xMid - 460, yMid - 25 , .0625, .0625);
+    //new Road(stage, finishAsset, 0.5, xMid - 165, yMid - 199, .0625, .0625, 90);
 
     // Car
-    addObject(stage, car, 0.5, xMid - 500, yMid - 40);
+    addObject(stage, car, 0.5, xMid - 460, yMid + 16);
     car.width = 32;
     car.height = 32;
 }
